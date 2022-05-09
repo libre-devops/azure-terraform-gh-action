b9f400c6fcb20f94d1edabae4878c94bba8843ab
Craig Thacker <craigthackerx@gmail.com>
2022-05-09 22:09:21 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,84 +21,135 @@ print_alert() {
 rm -rf .terraform && \
 mkdir -p .terraform && \
 
-if [ -z ${1} ]; then
-    code_path="${1}" && \
-    cd "${code_path}" || print_error "Code path is empty or invalid" && exit 1
+if [ -z "${1}" ]; then
+    terraform_path="${1}" && \
+    cd "${terraform_path}" || print_error "Code path is empty or invalid" && exit 1
     else
       print_error "Code path is empty or invalid" && exit 1
 fi
 
-if [ -z ${2} ]; then
-    workspace_name="${2}" && \
-    printf '%s' "${workspace}" > .terraform/environment || print_error "Workspace name failed to create" && exit 1
+if [ -z "${2}" ]; then
+    terraform_workspace_name="${2}" && \
+    printf '%s' "${terraform_workspace_name}" > .terraform/environment || print_error "Workspace name failed to create" && exit 1
     else
-      print_alert "No terraform workspace has been applied, continuing..."
+       print_error "Workspace name failed to create" && exit 1
 fi
 
-if [ -z ${3} ]; then
-    backend_sa_rg_name="${3}" || print_error "Variable assignment for backend storage account resource group failed or is invalid" && exit 1
+if [ -z "${3}" ]; then
+    terraform_backend_sa_rg_name="${3}" || print_error "Variable assignment for backend storage account resource group failed or is invalid" && exit 1
     else
       print_error "Variable assignment for backend storage account resource group failed or is invalid" && exit 1
 fi
 
-if [ -z ${4} ]; then
-    backend_sa_name="${4}" || print_error "Variable assignment for backend storage account name failed or is invalid" && exit 1
+if [ -z "${4}" ]; then
+    terraform_backend_sa_name="${4}" || print_error "Variable assignment for backend storage account name failed or is invalid" && exit 1
     else
       print_error "Variable assignment for backend storage account name failed or is invalid" && exit 1
 fi
 
-if [ -z ${5} ]; then
-    backend_blob_container_name="${5}" || print_error "Variable assignment for backend storage account blob container failed or is invalid" && exit 1
+if [ -z "${5}" ]; then
+    terraform_backend_blob_container_name="${5}" || print_error "Variable assignment for backend storage account blob container failed or is invalid" && exit 1
     else
       print_error "Variable assignment for backend storage account blob container failed or is invalid" && exit 1
 fi
 
-if [ -z ${6} ]; then
-    backend_state_name="${6}" || print_error "Variable assignment for backend state name failed or is invalid" && exit 1
+if [ -z "${6}" ]; then
+    terraform_backend_storage_access_key="${6}" || print_error "Variable assignment for backend storage access key name failed or is invalid" && exit 1
+    else
+      print_error "Variable assignment for backend storage access key name has failed or is invalid" && exit 1
+fi
+
+if [ -z "${7}" ]; then
+    terraform_backend_state_name="${7}" || print_error "Variable assignment for backend state name failed or is invalid" && exit 1
     else
       print_error "Variable assignment for backend state name has failed or is invalid" && exit 1
 fi
 
-if [ -z ${7} ]; then
-    provider_client_id="${7}" || print_error "Variable assignment for provider client id failed or is invalid" && exit 1
+if [ -z "${8}" ]; then
+    terraform_provider_client_id="${8}" || print_error "Variable assignment for provider client id failed or is invalid" && exit 1
     else
       print_error "Variable assignment for provider client id has failed or is invalid" && exit 1
 fi
 
-if [ -z ${8} ]; then
-    provider_client_secret="${8}" || print_error "Variable assignment for provider client secret failed or is invalid" && exit 1
+if [ -z "${9}" ]; then
+    terraform_provider_client_secret="${9}" || print_error "Variable assignment for provider client secret failed or is invalid" && exit 1
     else
       print_error "Variable assignment for provider client secret has failed or is invalid" && exit 1
 fi
 
 
-if [ -z ${8} ]; then
-    provider_client_subscription_id="${8}" || print_error "Variable assignment for provider subscription id failed or is invalid" && exit 1
+if [ -z "${10}" ]; then
+    terraform_provider_client_subscription_id="${10}" || print_error "Variable assignment for provider subscription id failed or is invalid" && exit 1
     else
       print_error "Variable assignment for provider subscritpion id has failed or is invalid" && exit 1
 fi
 
-if [ -z ${8} ]; then
-    provider_client_subscription_id="${8}" || print_error "Variable assignment for provider subscription id failed or is invalid" && exit 1
+if [ -z "${11}" ]; then
+    terraform_provider_client_tenant_id="${11}" || print_error "Variable assignment for provider tenant id failed or is invalid" && exit 1
     else
-      print_error "Variable assignment for provider subscritpion id has failed or is invalid" && exit 1
+      print_error "Variable assignment for provider tenant id has failed or is invalid" && exit 1
 fi
 
+if [ -z "${12}" ]; then
+    terraform_compliance_path="${12}" || print_error "Terraform compliance path is invalid or empty" && exit 1
+    else
+      print_error "Terraform compliance path is invalid or empty" && exit 1
+fi
 
+if [ -z "${13}" ]; then
+    checkov_skipped_test="${13}" || print_error "Checkov Skipped  is invalid or empty" && exit 1
+    else
+    checkov_skipped_test="" || print_error "Checkov Skipped  is invalid or empty" && exit 1
+fi
+
+if [ -z "${14}" ]; then
+    run_terrafrom_destroy="${14}" || print_error "Checkov Skipped  is invalid or empty" && exit 1
+    else
+    print_error "Terraform destroy is empty" && exit 1
+fi
+
+export ARM_CLIENT_ID="${terraform_provider_client_id}"
+export ARM_CLIENT_SECRET="${terraform_provider_client_secret}"
+export ARM_SUBSCRIPTION_ID="${terraform_provider_client_subscription_id}"
+export ARM_TENANT_ID="${terraform_provider_client_tenant_id}"
+
+if [ "${run_terrafrom_destroy}" = "false" ]; then
 
 terraform init \
--backend-config="resource_group_name=${3}" \
--backend-config="storage_account_name=${4}" \
--backend-config="access_key=${4}" \
--backend-config="container_name=${5}" \
--backend-config="key=${TF_VAR_short}-${TF_VAR_env}.terraform.tfstate" && \
+-backend-config="resource_group_name=${terraform_backend_sa_rg_name}" \
+-backend-config="storage_account_name=${terraform_backend_sa_name}" \
+-backend-config="access_key=${terraform_backend_storage_access_key}" \
+-backend-config="container_name=${terraform_backend_blob_container_name}" \
+-backend-config="key=${terraform_backend_state_name}" && \
 
-printf '%s' "${TF_VAR_env}" > .terraform/environment && \
+terraform workspace select "${terraform_workspace_name}" && \
 
-terraform workspace select "${2}" && \
+terraform validate && \
 
-terraform plan -destroy -out ${PIPELINE_PLAN} && \
+terraform plan -out pipeline.plan && \
+terraform-compliance -p pipeline.plan -f "${terraform_compliance_path}" && \
+tfsec && \
+terraform show -json pipeline.plan > pipeline.plan.json && \
+checkov -f pipeline.plan.json --skip-check "${checkov_skipped_test}" && \
 
-terraform validate
+terraform apply -auto-approve pipeline.plan
 
+elif [ "${run_terrafrom_destroy}" = "true" ]; then
 
+    terraform init \
+-backend-config="resource_group_name=${terraform_backend_sa_rg_name}" \
+-backend-config="storage_account_name=${terraform_backend_sa_name}" \
+-backend-config="access_key=${terraform_backend_storage_access_key}" \
+-backend-config="container_name=${terraform_backend_blob_container_name}" \
+-backend-config="key=${terraform_backend_state_name}" && \
+
+terraform workspace select "${terraform_workspace_name}" && \
+
+terraform validate && \
+
+terraform plan -destroy -out pipeline.plan && \
+terraform apply -auto-approve pipeline.plan
+
+fi
+
+print_successn "Build ran sccessfully"
